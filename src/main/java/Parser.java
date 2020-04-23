5a9ff29e0989fc18a154c3714583d7d20784a43f
Stoncs <andrej2002322@gmail.com>
2020-04-23 16:26:58 +0300

    Разбиение по числу строк

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,6 +3,7 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,40 +16,48 @@ public class Parser {
     private int rows = -1;
 
     @Option(name = "-c", usage = "the size of the output file in characters", forbids = {"-l", "-n"})
-    private int size = -1;
+    private int characters = -1;
 
     @Option(name = "-n", usage = "number of output files", forbids = {"-l", "-c"})
-    private int number = -1;
+    private int files = -1;
 
     @Option(name = "-o", usage = "output name file", metaVar = "OUTPUT")
-    private String out;
+    private String out = "";
 
     @Argument
     private List<String> arguments = new ArrayList<String>();
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         new Parser().parseArgs(args);
     }
 
-    public void parseArgs(String [] args) {
+    public void parseArgs(String[] args) {
         CmdLineParser parser = new CmdLineParser(this);
         try {
             parser.parseArgument(args);
-            if (arguments.isEmpty() || (!arguments.get(0).equals("split")) || (rows > 0 && size > 0) ||
-                    (rows > 0 && number > 0) || (size > 0 && number > 0)) {
+            if (arguments.isEmpty() || (!arguments.get(0).equals("split")) || (rows > 0 && characters > 0) ||
+                    (rows > 0 && files > 0) || (characters > 0 && files > 0) || (arguments.size() != 2)) {
                 System.err.println("Error entering arguments (for correct input, see the example)");
                 System.err.println("split [options...] arguments...");
                 parser.printUsage(System.err);
-                System.err.println("\nExample: split [-d] [-l num | -c num | -n num] [-o outputname] inputname");
+                System.err.println("\nExample: split [-d] [-l num | -c num | -n num] [-o outputname] inputname.txt");
                 throw new IllegalArgumentException("");
             }
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
             System.err.println("split [options...] arguments...");
             parser.printUsage(System.err);
-            System.err.println("\nExample: split [-d] [-l num | -c num | -n num] [-o outputname] inputname");
+            System.err.println("\nExample: split [-d] [-l num | -c num | -n num] [-o outputname] inputname.txt");
             throw new IllegalArgumentException("");
         }
 
+        try {
+            if (rows > 0) SplitProject.numberOfLine(outputNames, rows, out, arguments.get(1));
+            if (rows > 0) SplitProject.numberOfCharacters(outputNames, characters, out, arguments.get(1));
+            if (rows > 0) SplitProject.numberOfFiles(outputNames, files, out, arguments.get(1));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("");
+        }
     }
 }
