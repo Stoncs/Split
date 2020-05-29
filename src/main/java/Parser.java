db331264906e8a264bd6ff7aab4113eada79585e
Stoncs <andrej2002322@gmail.com>
2020-05-29 20:21:29 +0300

    При разбиении по количеству символов (например, 100) в некоторых файлах получается по 99-97 Сделал разбиение по количеству файлов, записывая в него количество строк во входной файле / количество файлов, округлённое в большую сторону. Но тогда не получается при ситуации, когда строк меньше, чем файлов (например, 6 строк, 10 файлов), и когда числа близки друг к другу (например, 5 строк, 4 файла). Надо исправить.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,61 +3,100 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
 
 public class Parser {
+    private int numberOutputFile, prNumberOutputFile;
+    private int count;
+    private BufferedReader reader;
+    private  BufferedWriter writer;
+    String nameOfFile;
 
     @Option(name = "-d", usage = "sequence name")
-    private boolean outputNames = false;
+    private boolean flagD = false;
 
     @Option(name = "-l", usage = "number of rows (default: 100)", forbids = {"-c", "-n"})
-    private int rows = -1;
+    private int numberLines = -1;
 
     @Option(name = "-c", usage = "the size of the output file in characters", forbids = {"-l", "-n"})
-    private int characters = -1;
+    private int numberCharacters = -1;
 
     @Option(name = "-n", usage = "number of output files", forbids = {"-l", "-c"})
-    private int files = -1;
+    private int numberFile = -1;
 
     @Option(name = "-o", usage = "output name file", metaVar = "OUTPUT")
-    private String out = "";
+    private String outputName = "x";
 
-    @Argument
-    private List<String> arguments = new ArrayList<String>();
+    @Argument(required = true, usage = "Input file name", metaVar = "InputName")
+    String inputFileName;
 
-    public static void main(String[] args) {
-        new Parser().parseArgs(args);
+
+    private String getOutputNameCharChar() {
+        String name = outputName;
+        if (numberOutputFile < 27) name += "a" + (char) ((int) 'a' + numberOutputFile - 1);
+        else name += (char) ((int) 'a' + (numberOutputFile - 1) / 26) + "" +  (char) ((int) 'a' + (numberOutputFile - 1) % 26);
+        return name;
     }
 
-    public void parseArgs(String[] args) {
+    private void writeToFile(String text, int size) throws IOException {
+        if (numberOutputFile - prNumberOutputFile > 1 ) {
+            prNumberOutputFile++;
+            writer = new BufferedWriter(new FileWriter("Files\\" + nameOfFile));
+        }
+        writer.write(text);
+        count--;
+        if (count == 0) {
+            writer.close();
+            numberOutputFile++;
+            nameOfFile = flagD ? outputName + numberOutputFile : getOutputNameCharChar();
+            count = size;
+        }
+    }
+
+    private void workOutputFile(boolean line, int size) throws IOException {
+        int ch;
+        String initialName, str;
+        prNumberOutputFile = 0;
+        numberOutputFile = 1;
+        count = size;
+        initialName = flagD ? outputName + "1" : outputName + "aa";
+        writer = new BufferedWriter(new FileWriter("Files\\" + initialName));
+        if (line) {
+            while ((str = reader.readLine()) != null)
+                writeToFile(str + "\n", size);
+        }
+         else {
+            while ((ch = reader.read()) != -1)
+                writeToFile("" + (char) ch, size);
+        }
+    }
+
+    public void parseArgs(String[] args) throws IOException {
         CmdLineParser parser = new CmdLineParser(this);
+
+        count = 0;
         try {
             parser.parseArgument(args);
-            if (arguments.isEmpty() || (!arguments.get(0).equals("split")) || (rows > 0 && characters > 0) ||
-                    (rows > 0 && files > 0) || (characters > 0 && files > 0) || (arguments.size() != 2)) {
-                System.err.println("Error entering arguments (for correct input, see the example)");
-                System.err.println("split [options...] arguments...");
-                parser.printUsage(System.err);
-                System.err.println("\nExample: split [-d] [-l num | -c num | -n num] [-o outputname] inputname.txt");
-                throw new IllegalArgumentException("");
-            }
         } catch (CmdLineException e) {
             System.err.println(e.getMessage());
-            System.err.println("split [options...] arguments...");
-            parser.printUsage(System.err);
             System.err.println("\nExample: split [-d] [-l num | -c num | -n num] [-o outputname] inputname.txt");
+            parser.printUsage(System.err);
             throw new IllegalArgumentException("");
         }
-
-        try {
-            if (rows > 0) SplitProject.numberOfLine(outputNames, rows, out, arguments.get(1));
-            if (characters > 0) SplitProject.numberOfCharacters(outputNames, characters, out, arguments.get(1));
-            if (files > 0) SplitProject.numberOfFiles(outputNames, files, out, arguments.get(1));
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException("");
+        reader = new BufferedReader(new FileReader("Files\\" + inputFileName));
+        if (outputName.equals("-")) outputName = inputFileName;
+        if (numberLines != -1) workOutputFile(true, numberLines);
+        else if (numberCharacters != -1) workOutputFile(false, numberCharacters);
+        else if (numberFile != -1) {
+            int numberLinesInFile = 0;
+            while (reader.readLine() != null) numberLinesInFile++;
+            reader.close();
+            reader = new BufferedReader(new FileReader("Files\\" + inputFileName));
+            numberLines = (int) Math.ceil((double) numberLinesInFile / numberFile);
+            workOutputFile(true, numberLines);
         }
+        else workOutputFile(true, 100);
+        reader.close();
+        writer.close();
     }
 }
--- src/main/java/SplitProject.java
+++ src/main/java/SplitProject.java
@@ -1,131 +1,13 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 
-public class SplitProject {
-    public static void numberOfLine(boolean name, int numberOfLines, String outputName, String inputName) throws IOException {
-        File input = new File(inputName);
-        File output;
-        BufferedReader reader = null;
-        int count;      //количество строк в текущем файле
-        int number = 1;     //количество файлов выходных файлов
-        String line;
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        if (outputName.equals("")) outputName = "X";
-        if (outputName.equals("-")) {
-            int n = inputName.length();
-            outputName = inputName.substring(6, n - 4);
-        }
+
+class Main {
+    public static void main(String[] args){
+        Parser parser = new Parser();
         try {
-            reader = new BufferedReader(new FileReader(input));
-            if (name) {
-                while ((line = reader.readLine()) != null) {
-                    output = new File("Output\\" + outputName + number + ".txt");
-                    PrintWriter pw = new PrintWriter(output);
-                    pw.println(line);
-                    count = 1;
-                    while (count != numberOfLines && (line = reader.readLine()) != null) {
-                        pw.println(line);
-                        count++;
-                    }
-                    number++;
-                    pw.close();
-                }
-            } else {
-                while ((line = reader.readLine()) != null) {
-                    int first = (int) (double) number / 26;
-                    int second;
-                    if (first != 0) {
-                        second = number - first - first * 26;
-                    }
-                    else {
-                        second = number - 1;
-                    }
-                    output = new File("Output\\" + outputName + alphabet.charAt(first) +
-                            alphabet.charAt(second) + ".txt");
-                    PrintWriter pw = new PrintWriter(output);
-                    pw.println(line);
-                    count = 1;
-                    while (count != numberOfLines && (line = reader.readLine()) != null) {
-                        pw.println(line);
-                        count++;
-                    }
-                    number++;
-                    pw.close();
-                }
-            }
+            parser.parseArgs(args);
         } catch (IOException e) {
-            System.out.print("Error:" + e);
-        } finally {
-            assert reader != null;
-            reader.close();
+            e.printStackTrace();
         }
-    }
-    public static void numberOfCharacters(boolean name, int numberOfChars, String outputName, String inputName) throws IOException {
-        File input = new File(inputName);
-        File output;
-        BufferedReader reader = null;
-        int number = 1;     //количество файлов выходных файлов
-        int chars = 0;      //количество символов в текущем файле
-        int symbol;
-        String line;
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        if (outputName.equals("")) outputName = "X";
-        if (outputName.equals("-")) {
-            int n = inputName.length();
-            outputName = inputName.substring(6, n - 4);
-        }
-        try {
-            reader = new BufferedReader(new FileReader(input));
-            if (name) {
-                output = new File("Output\\" + outputName + number + ".txt");
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output), "UTF8"));
-                while ((symbol = reader.read()) != -1) {        //когда дойдём до конца файла, получим -1
-                    if (chars == numberOfChars) {
-                        number++;
-                        output = new File("Output\\" + outputName + number + ".txt");
-                        writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output), "UTF8"));
-                        chars = 0;
-                    }
-                    writer.write(symbol);
-                    writer.flush();
-                    chars++;
-                }
-                writer.close();
-            } else {
-                output = new File("Output\\" + outputName + "a" +
-                        "a" + ".txt");
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output), "UTF8"));
-                while ((symbol = reader.read()) != -1) {
-                    if (chars == numberOfChars) {
-                        number++;
-                        int first = (int) (double) number / 26;
-                        int second;
-                        if (first != 0) {
-                            second = number - first * 26;
-                        } else {
-                            second = number - 1;
-                        }
-                        output = new File("Output\\" + outputName + alphabet.charAt(first) +
-                                alphabet.charAt(second) + ".txt");
-                        writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output), "UTF8"));
-                        chars = 0;
-                    }
-                    writer.write(symbol);
-                    writer.flush();
-                    chars++;
-                }
-                writer.close();
-            }
-        } catch (IOException e) {
-            System.out.print("Error:" + e);
-        } finally {
-            assert reader != null;
-            reader.close();
-        }
-    }
-
-    public static void numberOfFiles(boolean name, int number, String outputName, String inputName) {
-
     }
 }
